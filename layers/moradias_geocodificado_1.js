1cbaf9b87901e4724b923033e1aa01b0add76f17
Samais Silva <samais.silva@advicehealth.com.br>
2025-10-23 14:32:33 -0300

    Implementacao final de todos os filtros

--- layers/moradias_geocodificado_1.js
+++
@@ -1,8 +0,0 @@
-var json_moradias_geocodificado_1 = {
-"type": "FeatureCollection",
-"name": "moradias_geocodificado_1",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-
-]
-}
